eab65c43fc9be7b8b5d3deb52016038621a3d405
Olivier Li <olivierli@google.com>
2020-09-08 08:16:31 -0400

    Format and convert second test

--- tests/layer_and_mod_hold_tap/test_layer_hold_mod_tap.cpp
+++ tests/layer_and_mod_hold_tap/test_layer_hold_mod_tap.cpp
@@ -1,84 +1,80 @@
 #include "test_common.hpp"
-#include "action_tapping.h"
 #include "layer_with_mod_tap.h"
+#include "action_tapping.h"
 
 using testing::_;
 using testing::InSequence;
 
 class LayerModHoldTapTest : public TestFixture {
-  protected:
-    void ExpectActivation(){
-      // Noop triggered by layer change.
-      ExpectKeys();
-      // Expect modifier turned on.
-      ExpectKeyPressed(KC_LSFT);
-    }
+ protected:
+  void ExpectActivation() {
+    // Noop triggered by layer change.
+    ExpectKeys();
+    // Expect modifier turned on.
+    ExpectKeyPressed(KC_LSFT);
+  }
 
-    void ExpectDeactivation(){
-      // Expect modifier turned off.
-      ExpectKeyReleased(KC_LSFT);
-      // Noop triggered by layer change.
-      ExpectKeys();
-    }
+  void ExpectDeactivation() {
+    // Expect modifier turned off.
+    ExpectKeyReleased(KC_LSFT);
+    // Noop triggered by layer change.
+    ExpectKeys();
+  }
 
-    void ExpectKeys(){
-      auto key_matcher = testing::MakeMatcher(new KeyboardReportMatcher(keys));
-      EXPECT_CALL(driver, send_keyboard_mock(key_matcher));
-    }
+  void ExpectKeys() {
+    auto key_matcher = testing::MakeMatcher(new KeyboardReportMatcher(keys));
+    EXPECT_CALL(driver, send_keyboard_mock(key_matcher));
+  }
 
-    void ExpectKeyReleased(uint8_t key){
-      auto it = std::find(keys.begin(), keys.end(), key);
-      EXPECT_NE(it, keys.end());
-      keys.erase(it);
+  void ExpectKeyReleased(uint8_t key) {
+    auto it = std::find(keys.begin(), keys.end(), key);
+    EXPECT_NE(it, keys.end());
+    keys.erase(it);
 
-      ExpectKeys();
-    }
+    ExpectKeys();
+  }
 
-    void ExpectKeyPressed(uint8_t key){
-      auto it = std::find(keys.begin(), keys.end(), key);
-      EXPECT_EQ(it, keys.end());
-      keys.push_back(key);
+  void ExpectKeyPressed(uint8_t key) {
+    auto it = std::find(keys.begin(), keys.end(), key);
+    EXPECT_EQ(it, keys.end());
+    keys.push_back(key);
 
-      ExpectKeys();
-    }
+    ExpectKeys();
+  }
 
-    void ExpectTap(uint8_t key){
-      ExpectKeyPressed(key);
-      ExpectKeyReleased(key);
-    }
+  void ExpectTap(uint8_t key) {
+    ExpectKeyPressed(key);
+    ExpectKeyReleased(key);
+  }
 
-    void ExpectNoMorePresses(){
-      EXPECT_CALL(driver, send_keyboard_mock(_)).Times(0);
-    }
+  void ExpectNoMorePresses() {
+    EXPECT_CALL(driver, send_keyboard_mock(_)).Times(0);
+  }
 
-    TestDriver driver;
-    InSequence s;
-    std::vector<uint8_t> keys;
+  TestDriver driver;
+  InSequence s;
+  std::vector<uint8_t> keys;
 };
 
-enum class Position {
-  DOWN,
-  UP
-};
+enum class Position { DOWN, UP };
 
 class ScopedPhysicalKeyPress {
-  public:
-    explicit ScopedPhysicalKeyPress(LayerModHoldTapTest* test,
-        uint8_t col, uint8_t row, Position position):test_(test){
-      if(position == Position::DOWN){
-        press_key(col, row);
-      }
-      else {
-        release_key(col, row);
-      }
-    }
-    ~ScopedPhysicalKeyPress(){
-      test_->run_one_scan_loop();
-    }
-  private:
-    LayerModHoldTapTest* test_;
-};
+ public:
+  explicit ScopedPhysicalKeyPress(LayerModHoldTapTest* test, uint8_t col,
+                                  uint8_t row, Position position) : test_(test) {
+    assert(test);
 
+    if (position == Position::DOWN) {
+      press_key(col, row);
+    } else {
+      release_key(col, row);
+    }
+  }
+  ~ScopedPhysicalKeyPress() { test_->run_one_scan_loop(); }
+
+ private:
+  LayerModHoldTapTest* test_;
+};
 
 TEST_F(LayerModHoldTapTest, PressingForLessThanTappingTermResultsInTap) {
   {
@@ -94,22 +90,18 @@ TEST_F(LayerModHoldTapTest, PressingForLessThanTappingTermResultsInTap) {
 }
 
 TEST_F(LayerModHoldTapTest, PressingForMoreThanTappingTermResultsIsNoop) {
-  // Press the key.
-  press_key(7, 0);
-  ExpectActivation();
-
-  run_one_scan_loop();
+  {
+    ScopedPhysicalKeyPress down(this, 7, 0, Position::DOWN);
+    ExpectActivation();
+  }
 
   idle_for(TAPPING_TERM);
 
-  // Then the release
-  release_key(7, 0);
-
-  ExpectDeactivation();
-
-  // No tap since release happened outside of
-  // tapping term.
-  ExpectNoMorePresses();
-
-  run_one_scan_loop();
+  {
+    ScopedPhysicalKeyPress down(this, 7, 0, Position::UP);
+    ExpectDeactivation();
+    // No tap since release happened outside of
+    // tapping term.
+    ExpectNoMorePresses();
+  }
 }
